f3225bacb740742de463bb83987701100ed9c366
eebbesen <eebbesen.git@gmail.com>
2024-03-05 15:50:29 -0600

    Replace toggle icon with descriptive text

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { CogIcon } from "@heroicons/react/16/solid";
-
 export default function Navbar({
   distanceType,
   setDistanceType,
@@ -9,22 +7,18 @@ export default function Navbar({
   distanceType: Distance;
   setDistanceType: Function;
 }) {
+  const newDistanceType = distanceType === "miles" ? "kms" : "miles";
   const clickHandler = () => {
-    const newDistanceType = distanceType === "miles" ? "kms" : "miles";
     setDistanceType(newDistanceType);
   };
 
   return (
-    <header className="text-right justify-end grid my-2 me-6">
+    <header className="text-right my-2 me-6">
       <nav>
         <ul>
-          <li>
-            <CogIcon
-              className="text-white hover:text-greek-blue h-6 w-6 transition ease-in-out delay-175"
-              onClick={clickHandler}
-            />
-            {distanceType}
-          </li>
+          <a href="#" onClick={clickHandler}>
+            {`View in ${newDistanceType}`}
+          </a>
         </ul>
       </nav>
     </header>
