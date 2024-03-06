3db624cc5f52a43791d2c39268d7b3f3cb4fcee7
J2EEbbesen <eebbesen@users.noreply.github.com>
2024-03-06 17:03:55 -0600

    testing useEffect in Home (#17)

--- __tests__/src/app/home.test.tsx
+++ __tests__/src/app/home.test.tsx
@@ -2,17 +2,39 @@ import Home from "../../../src/app/page";
 import "@testing-library/jest-dom";
 import { act } from "react-dom/test-utils";
 import { render, screen } from "@testing-library/react";
-import { mockFetch, mockGeolocation } from "./testHelper";
+import * as testHelper from "./testHelper";
 
 describe("Home", () => {
-  it("should render loading when no lat/long", async () => {
-    window.fetch = mockFetch([]);
-    mockGeolocation(0.0, 0.0);
+  it("should render loading when lat is zero", async () => {
+    window.fetch = testHelper.mockFetch([]);
+    testHelper.mockGeolocation(0.0, -93.169022);
 
     await act(() => {
       render(<Home />);
     });
 
     expect(screen.getByTestId("loading")).toBeInTheDocument();
+  });
+
+  it("should render loading when lng is zero", async () => {
+    window.fetch = testHelper.mockFetch([]);
+    testHelper.mockGeolocation(44.938128, 0.0);
+
+    await act(() => {
+      render(<Home />);
+    });
+
+    expect(screen.getByTestId("loading")).toBeInTheDocument();
+  });
+
+  it("should render data when lat/long", async () => {
+    window.fetch = testHelper.mockFetch(JSON.parse(testHelper.BUSINESSES_JSON));
+    testHelper.mockGeolocation(44.938128, -93.169022);
+
+    await act(() => {
+      render(<Home />);
+    });
+
+    expect(screen.getByText("Shish")).toBeInTheDocument();
   });
 });
